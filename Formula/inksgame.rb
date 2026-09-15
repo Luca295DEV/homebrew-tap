08533843662262b76b7d24e25ffd93e916670d4a
Luca295 <lucaschnell@aol.com>
2026-09-16 01:28:47 +0200

    back hurts bye

--- Formula/inksgame.rb
+++ Formula/inksgame.rb
@@ -1,21 +1,30 @@
 class Inksgame < Formula
   desc "Design games in Inkscape, apply logic in your engine of choice."
   homepage "https://codeberg.org/Luca295/inksgame"
-  url "https://codeberg.org/Luca295/inksgame/archive/refs/tags/beta5.tar.gz"
   version "beta5"
   license "MIT"
-  sha256 "9c87f65ad3ef02d166a5dc815db25c496c8434e5dc47b254d5d7f04179b3d3b3"
-  depends_on "go" => :build
 
-  bottle do
-    root_url "https://github.com/Luca295DEV/homebrew-tap"
+  releasesLink "https://codeberg.org/Luca295/inksgame/releases"
+
+  on_macos do
+    on_arm do
+      url releasesLink+"/inksgame-darwin-arm64.zip"
+    end
+    on_intel do
+      url releasesLink+"/inksgame-darwin-amd64.zip"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url releasesLink+"/inksgame-linux-arm64.zip"
+    end
+    on_intel do
+      url releasesLink+"/inksgame-linux-amd64.zip"
+    end
   end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-X main.version=#{version}")
-  end
-
-  test do
-    assert_match version.to_s, shell_output("#{bin}/inksgame --version 2>&1")
+    bin.install "inksgame"
   end
 end
