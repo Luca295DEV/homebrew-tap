4c00f40f77ef18419432715cb522522984a46cfc
Luca295 <lucaschnell@aol.com>
2026-09-21 11:59:01 +0200

    Mon Sep 21 2026 11:59:01 GMT+0200 (Central European Summer Time)

--- Formula/inksgame.rb
+++ Formula/inksgame.rb
@@ -1,30 +1,28 @@
 class Inksgame < Formula
-  desc "Design games in Inkscape, apply logic in your engine of choice."
+  desc "Design games in Inkscape, apply logic in your engine of choice"
   homepage "https://codeberg.org/Luca295/inksgame"
   version "beta5"
   license "MIT"
 
-  RELEASES_LINK = "https://codeberg.org/Luca295/inksgame/releases/download/#{version}".freeze
-
   on_macos do
     on_arm do
-      url "#{RELEASES_LINK}/inksgame-darwin-arm64.zip"
-      sha256 "fff0edb3dffd27c1ece0ae959e819d7e29820d19cf62a1c64f6e803c184f3844"
+      url "https://codeberg.org/Luca295/inksgame/releases/download/beta5/inksgame-darwin-arm64.zip"
+      sha256 "a78ddf0ccad4ad9e32a499aee923e5225a26042b930c2b4e3923d2e472e9ec18"
     end
     on_intel do
-      url "#{RELEASES_LINK}/inksgame-darwin-amd64.zip"
-      sha256 "0cb39688b4543209b7bdd7cdd2b86dc6630ff7719e1e159a0563b721fb134a5f"
+      url "https://codeberg.org/Luca295/inksgame/releases/download/beta5/inksgame-darwin-amd64.zip"
+      sha256 "1303b2b97e96c23c8fbaa05e5ab4d861eed07d5285c59dc4b9dcb48d833b5d6e"
     end
   end
 
   on_linux do
     on_arm do
-      url "#{RELEASES_LINK}/inksgame-linux-arm64.zip"
-      sha256 "70fc2efca6de3dae24f42c7291d4157949d4c97f836964c637d1b40442cb48e7"
+      url "https://codeberg.org/Luca295/inksgame/releases/download/beta5/inksgame-linux-arm64.zip"
+      sha256 "e67433cf61eaa4680d36540ce6b9dbc3feb92d9ca0712d05f364df1d985c59f8"
     end
     on_intel do
-      url "#{RELEASES_LINK}/inksgame-linux-amd64.zip"
-      sha256 "19b597e6907bd63e10b36ac75ba1c0c2ba3d104a18446953c40fb23c5942dae0"
+      url "https://codeberg.org/Luca295/inksgame/releases/download/beta5/inksgame-linux-amd64.zip"
+      sha256 "ae3dcddb091d067f694abfc6cc6f0070d9ccf0b94b9a808f93403b8f4978454e"
     end
   end
 
